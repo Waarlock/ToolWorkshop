e261d553cd50b45205af9a8f696aae97cfbe251e
pablo <pablo@DESKTOP-FRH24EN>
2022-04-16 04:39:11 -0500

    Categories CRUD Finished

--- ToolWorkshop/ToolWorkshop/Data/DataContext.cs
+++ ToolWorkshop/ToolWorkshop/Data/DataContext.cs
@@ -10,11 +10,13 @@ namespace ToolWorkshop.Data
         }
 
         public DbSet<Country> countries { get; set; }
+        public DbSet<Category> categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
--- ToolWorkshop/ToolWorkshop/Data/Entities/Category.cs
+++ ToolWorkshop/ToolWorkshop/Data/Entities/Category.cs
@@ -2,13 +2,12 @@
 
 namespace ToolWorkshop.Data.Entities
 {
-    public class Country
+    public class Category
     {
         public int Id { get; set; }
-        [Display (Name = "País")]
+        [Display(Name = "Categoría")]
         [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
         public string Name { get; set; }
-
     }
 }
