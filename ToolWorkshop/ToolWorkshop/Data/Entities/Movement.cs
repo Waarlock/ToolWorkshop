c3d77cc7a61b2fe5ec8759be86a12297fb449738
EDGAR ANDRES MARTINEZ ZAPATA <edgarmartinez229064@correo.itm.edu.co>
2022-04-23 17:55:31 -0500

    Entities

--- ToolWorkshop/ToolWorkshop/Data/Entities/Movement.cs
+++ ToolWorkshop/ToolWorkshop/Data/Entities/Movement.cs
@@ -1,6 +1,27 @@
-﻿namespace ToolWorkshop.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using ToolWorkshop.Enums;
+
+namespace ToolWorkshop.Data.Entities
 {
     public class Movement
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Fecha de Inicio")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
+        public DateTime Start_DateTime { get; set; }
+
+        [Display(Name = "Fecha de Finalizacion")]
+        public DateTime End_DateTime { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
+        MovementStatus status { get; set; }
+
+        [Display(Name = "Usuario")]
+        [Required(ErrorMessage = "El Campo {0} es obligatorio.")]
+        public User UserId { get; set; }
+
+
     }
 }
--- ToolWorkshop/ToolWorkshop/Data/Entities/Role_User.cs
+++ ToolWorkshop/ToolWorkshop/Data/Entities/Role_User.cs
@@ -2,5 +2,10 @@
 {
     public class Role_User
     {
+        public int Id { get; set; }
+
+        public Role RoleId { get; set; }
+
+        public User UserId { get; set; }
     }
 }
