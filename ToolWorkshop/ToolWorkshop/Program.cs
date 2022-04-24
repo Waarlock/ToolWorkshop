da60934eb36b7ff1c7e10c72b6aec4e17dd7d5df
pablo <pablo@DESKTOP-FRH24EN>
2022-04-23 19:21:08 -0500

    login finished

--- ToolWorkshop/ToolWorkshop/Program.cs
+++ ToolWorkshop/ToolWorkshop/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ToolWorkshop.Data;
+using ToolWorkshop.Data.Entities;
+using ToolWorkshop.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +13,38 @@ builder.Services.AddDbContext<DataContext>(o =>
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+//TODO: make strongest password
+builder.Services.AddIdentity<User, IdentityRole>(cfg =>
+{
+    cfg.User.RequireUniqueEmail = true;
+    cfg.Password.RequireDigit = false;
+    cfg.Password.RequiredUniqueChars = 0;
+    cfg.Password.RequireLowercase = false;
+    cfg.Password.RequireNonAlphanumeric = false;
+    cfg.Password.RequireUppercase = false;
+    //cfg.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<DataContext>();
+
+
+builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+
 var app = builder.Build();
+
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service.SeedAsync().Wait();
+    }
+}
+
+
 
 if (!app.Environment.IsDevelopment())
 {
@@ -21,6 +55,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
