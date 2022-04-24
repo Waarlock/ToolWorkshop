167dc3dc08d1e79b6a7f0917718087c2142b0f05
EDGAR ANDRES MARTINEZ ZAPATA <edgarmartinez229064@correo.itm.edu.co>
2022-04-24 05:05:18 -0500

    Inyeccion de dependencias

--- ToolWorkshop/ToolWorkshop/Program.cs
+++ ToolWorkshop/ToolWorkshop/Program.cs
@@ -32,6 +32,9 @@ builder.Services.AddIdentity<User, IdentityRole>(cfg =>
 
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddScoped<ICombosHelper, CombosHelper>();
+builder.Services.AddScoped<IBlobHelper, BlobHelper>();
+builder.Services.AddScoped<IMailHelper, MailHelper>();
 
 var app = builder.Build();
 
