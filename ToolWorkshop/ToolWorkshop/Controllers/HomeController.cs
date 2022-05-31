df56fb8822f0973a8d7a4674bc76cd3701cbd7f3
pablo <pablo@DESKTOP-FRH24EN>
2022-05-31 00:20:56 -0500

    Home Screen

--- ToolWorkshop/ToolWorkshop/Controllers/HomeController.cs
+++ ToolWorkshop/ToolWorkshop/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using ToolWorkshop.Data;
+using ToolWorkshop.Data.Entities;
 using ToolWorkshop.Models;
 
 namespace ToolWorkshop.Controllers
@@ -7,21 +10,57 @@ namespace ToolWorkshop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Tool>? tools = await _context.Tools
+                .Include(t => t.ToolImages)
+                .Include(t => t.ToolCategories)
+                .OrderBy(t => t.Description)
+                .ToListAsync();
+
+            List<ToolsHomeViewModel> toolsHome = new() { new ToolsHomeViewModel() };
+            int i = 1;
+            foreach (Tool? tool in tools)
+            {
+                if (i == 1)
+                {
+                    toolsHome.LastOrDefault().Tool1 = tool;
+                }
+                if (i == 2)
+                {
+                    toolsHome.LastOrDefault().Tool2 = tool;
+                }
+                if (i == 3)
+                {
+                    toolsHome.LastOrDefault().Tool3 = tool;
+                }
+                if (i == 4)
+                {
+                    toolsHome.LastOrDefault().Tool4 = tool;
+                    toolsHome.Add(new ToolsHomeViewModel());
+                    i = 0;
+                }
+                i++;
+            }
+
+            return View(toolsHome);
+
         }
+
 
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ToolWorkshop/ToolWorkshop/Models/ToolsHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ToolWorkshop.Data.Entities;
+
+namespace ToolWorkshop.Models
+{
+    public class ToolsHomeViewModel
+    {     
+            public Tool Tool1 { get; set; }
+
+            public Tool Tool2 { get; set; }
+
+            public Tool Tool3 { get; set; }
+
+            public Tool Tool4 { get; set; }
+        
+    }
+}
